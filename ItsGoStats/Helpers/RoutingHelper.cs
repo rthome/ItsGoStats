24dcde873607820c3fd288cf908bcc9193b031d8
Raffael Thome <mail@rmt.name>
2018-01-06 19:06:36 +0100

    Make PlayerLink helper work

--- ItsGoStats/Helpers/RoutingHelper.cs
+++ ItsGoStats/Helpers/RoutingHelper.cs
@@ -1,19 +1,18 @@
-﻿using System;
-using System.Web;
-using ItsGoStats.Caching.Entities;
+﻿using ItsGoStats.Caching.Entities;
 using ItsGoStats.Routing;
+using Nancy.ViewEngines.Razor;
 
 namespace ItsGoStats.Helpers
 {
     public class RoutingHelper
     {
-        public HtmlString PlayerLink(Player player, string cssClasses = null, string dateConstraint = null)
+        public static IHtmlString PlayerLink(Player player, string cssClasses = null, string dateConstraint = null)
         {
             string classesFragment = null;
             if (cssClasses != null)
                 classesFragment = $"class=\"{cssClasses}\"";
-            var markup = $"<a {classesFragment} href=\"{PlayerModule.BasePath}/{player.SteamId}/{dateConstraint ?? ""}\">{player.Name}</a>";
-            return new HtmlString(markup);
+            var markup = $"<a {classesFragment} href=\"{PlayerModule.BasePath}/{player.SteamId}/{dateConstraint}\">{player.Name}</a>";
+            return new NonEncodedHtmlString(markup);
         }
     }
 }
