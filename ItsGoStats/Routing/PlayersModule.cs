ea1ede9f3ad23e5e2047b4789ee8ad4d327e65c8
Raffael Thome <mail@rmt.name>
2018-01-07 21:59:31 +0100

    Small tweaks

--- ItsGoStats/Routing/PlayersModule.cs
+++ ItsGoStats/Routing/PlayersModule.cs
@@ -24,7 +24,7 @@ namespace ItsGoStats.Routing
                 var models = new List<PlayerModel>();
                 foreach (var player in players)
                     models.Add(await PlayerModel.CreateAsync(player, DateConstraint.AllTime));
-                models.Sort((a, b) => Nullable.Compare(b.FirstSeen, a.FirstSeen)); // desc
+                models.Sort((a, b) => Nullable.Compare(a.FirstSeen, b.FirstSeen)); // desc
 
                 ViewBag.Date = DateConstraint.AllTime;
                 return View["players.cshtml", models];
