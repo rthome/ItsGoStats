05b6a17d1bff932a05c2665e2be1f3721daac34d
Raffael Thome <mail@rmt.name>
2018-01-08 21:39:59 +0100

    Properly query player list stats for date constraints

--- ItsGoStats/Routing/PlayersModule.cs
+++ ItsGoStats/Routing/PlayersModule.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
+using Dapper;
 using Dapper.Contrib.Extensions;
 
 using ItsGoStats.Caching.Entities;
@@ -15,17 +17,49 @@ namespace ItsGoStats.Routing
     {
         public const string BasePath = "/Players";
 
+        async Task<List<PlayerModel>> CreateModelsAsync(DateConstraint constraint)
+        {
+            const string PlayerQuery = @"
+                select Player.* from Player
+                inner join (
+                    select Kill.KillerId, max(date(Kill.Time)) as Time from Kill
+                    group by Kill.KillerId) LastKill on Player.Id = LastKill.KillerId
+                where LastKill.Time >= @Start and LastKill.Time < @End";
+
+            IEnumerable<Player> players;
+            if (constraint.Start == DateTime.MinValue && constraint.End == DateTime.MaxValue)
+                players = await DatabaseProvider.Connection.GetAllAsync<Player>();
+            else
+                players = await DatabaseProvider.Connection.QueryAsync<Player>(PlayerQuery, new { constraint.Start, constraint.End });
+
+            var models = new List<PlayerModel>();
+            foreach (var player in players)
+                models.Add(await PlayerModel.CreateAsync(player, constraint));
+            return models;
+        }
+
         public PlayersModule()
             : base(BasePath)
         {
-            Get["/", runAsync: true] = async (_, token) =>
+            Get["/"] = _ =>
             {
-                var players = await DatabaseProvider.Connection.GetAllAsync<Player>();
-                var models = new List<PlayerModel>();
-                foreach (var player in players)
-                    models.Add(await PlayerModel.CreateAsync(player, DateConstraint.AllTime));
+                return Response.AsRedirect("/Players/Today");
+            };
 
-                ViewBag.Date = DateConstraint.AllTime;
+            Get["/{Date:dateform}", runAsync: true] = async (parameters, token) =>
+            {
+                var models = await CreateModelsAsync((DateConstraint)parameters.Date);
+
+                ViewBag.Date = parameters.Date;
+                return View["players.cshtml", models];
+            };
+
+            Get["/From/{StartDate:dateform}/To/{EndDate:dateform}", runAsync: true] = async (parameters, token) =>
+            {
+                var date = DateConstraint.Merge((DateConstraint)parameters.StartDate, (DateConstraint)parameters.EndDate);
+                var models = await CreateModelsAsync(date);
+
+                ViewBag.Date = date;
                 return View["players.cshtml", models];
             };
         }
